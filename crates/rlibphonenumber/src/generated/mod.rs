701d596b9630d1fb91407b945a8bf19a838e736d
Vladislav K <vlados874@yandex.ru>
2026-02-23 13:07:10 +0000

    [fix] Deprecation warnings in generated code

--- crates/rlibphonenumber/src/generated/mod.rs
+++ crates/rlibphonenumber/src/generated/mod.rs
@@ -13,5 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod proto;
 pub mod metadata;
+#[allow(deprecated)]
+pub mod proto;
